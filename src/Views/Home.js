108c1b9a80d645f0c016d5f38553d01118d5c454
Andrii Mulyk <amulyk@lectrum.io>
2019-10-24 20:20:46 +0300

    add files

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { SmartNews } from '../Components/SmartNews';
 
 export const Home = () => {
   return (
     <>
         <h1>Home</h1>
-        <SmartNews />
     </>
   )
 };
