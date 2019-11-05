c2295df9c09663951582ac12123c959e56e06643
Andrii Mulyk <amulyk@lectrum.io>
2019-11-05 23:44:10 +0400

    add files

--- src/Components/Swapi/index.js
+++ src/Components/Swapi/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Starships } from '../Starships';
 import { Planets } from '../Planets';
+import { useLoading } from './hooks/useLoading';
 
 export const Swapi = () => {
+  const { isDataLoading } = useLoading();
+  const status = isDataLoading && 'NOT READY';
+
   return (
     <>
-      <h1>Swapi</h1>
+      <h1>Swapi {status}</h1>
       <Starships />
       <Planets />
     </>
