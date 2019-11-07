a08b2ccc8ddc06408e91e79e3caef9e1cf7ed6f9
Andrii Mulyk <amulyk@lectrum.io>
2019-11-07 21:35:50 +0200

    add files

---
+++ src/components/example-3/useCounterManager/index.test.js
@@ -0,0 +1,31 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useCounterManager } from './';
+
+describe('useCounterManager custom hook', () => {
+    test('Initial value should be the same as count value from returned object', () => {
+        const source = 0;
+        const returnedData = renderHook(() => useCounterManager(source));
+        const counterValue = returnedData.result.current.count; 
+        expect(counterValue).toBe(source);
+    });
+    test('Increase function should increase value by 1', () => {
+        const source = 0;
+        const returnedData = renderHook(() => useCounterManager(source));
+        const prevCount = returnedData.result.current.count;
+        const increase = returnedData.result.current.increase;
+        const incrementedValue = prevCount + 1;
+        act(increase);
+        const receivedValue = returnedData.result.current.count;
+        expect(receivedValue).toBe(incrementedValue);
+    });
+    test('Decrease function should decrease value by 1', () => {
+        const source = 0;
+        const returnedData = renderHook(() => useCounterManager(source));
+        const prevCount = returnedData.result.current.count;
+        const decrease = returnedData.result.current.decrease;
+        const decrementedValue = prevCount - 1;
+        act(decrease);
+        const receivedValue = returnedData.result.current.count;
+        expect(receivedValue).toBe(decrementedValue);
+    });
+});
